a757b29bd0438240953c1d792c52e51b4140c9bd
Hunny Chawla <hunnychawla528@gmail.com>
2025-12-20 20:16:21 +0530

    update logo url path

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -118,7 +118,7 @@ export default function LoginPage() {
             <div className="flex flex-col items-center justify-center space-y-4">
               <div className="flex items-center justify-center">
                 <Image
-                  src="/login-logo.png"
+                  src="./login-logo.png"
                   alt="CURA Logo"
                   width={691}
                   height={361}
